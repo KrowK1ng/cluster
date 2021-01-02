366ef93beb8ad29ed781a777a2aea4d5c7f485a7
KrowK1ng <victor.vorona2004@gmail.com>
2021-01-02 22:00:13 +0200

    added function string_parser: makes the right layout of the message

--- main.cpp
+++ main.cpp
@@ -3,15 +3,66 @@
 #include<thread>
 #include<vector>
 #include<string>
+#include <algorithm>
 
 #include"ftxui/component/input.hpp"
 #include"ftxui/component/screen_interactive.hpp"
 #include"ftxui/component/container.hpp"
 #include"ftxui/component/menu.hpp"
+#include"ftxui/component/button.hpp"
 #include"ftxui/screen/string.hpp"
+#include"ftxui/screen/terminal.hpp"
 #include"ftxui/dom/elements.hpp"
 
 using namespace ftxui;
+
+int screenw, screenh;
+
+Element string_parser(std::string message, int limitsize){
+    std::vector<std::string>line;                   //vector of lines
+    std::vector<Element>lines;
+    std::string temp_string;
+    
+    int i;                                          //faster for "for" loop
+    int clines = 1;                                 //number of lines
+    /*for(char c : message){
+        if(c == ' '){    //checks if space
+            if(findword){
+                temp_string = message.substr(lastpos,position - lastpos)
+            }
+        }
+    }*/
+
+    while(message.length() > limitsize){
+        if(message[limitsize] == ' '){      //if line ends with space
+            temp_string = message.substr(0,limitsize);
+            message = message.substr(limitsize);
+        }else{
+            for(i = limitsize - 1; i >= 0; i--){
+                if(message[i] == ' '){
+                    temp_string = message.substr(0,i);
+                    message = message.substr(i + 1);
+                    i = -2;
+                }
+            }
+            if(i == -1){
+                temp_string = message.substr(0,limitsize);
+                message = message.substr(limitsize);
+            }
+        }
+        
+        line.push_back(temp_string);
+        clines++;
+    }
+    line.push_back(message);
+
+    for(i = 0; i < clines; i++){
+        lines.push_back(text(to_wstring(line[i])));
+    }
+
+    return vbox(lines);
+}
+
 class Message {
     public:
         std::string sender;
@@ -39,18 +90,25 @@ class Messages : public Component {
             temp.message = "Hello indeed";
             messages.push_back(temp);
             temp.sender = "Jora";
-            temp.message = "I am your father aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ";
+            //temp.message = "I am your father";
+            temp.message = "I am your father aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ";
+            messages.push_back(temp);
+            temp.sender = "Lcuk";
+            temp.message = "Here is the alphabet abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
             messages.push_back(temp);
             for(int i = 0; i < messages.size(); i++){
-                //temp_message = hflow(paragraph(to_wstring(messages[i].Output())));
-                temp_message = text(to_wstring(messages[i].Output()));
-                emessages.push_back(temp_message);
+                emessages.push_back(string_parser(messages[i].Output(), screenw - 2));
             }
-            emessages.push_back(filler());
         }
         
         Element Render() override{
-            return window(text(L"Nerd Club") ,vbox(emessages));
+            /*for(int i = 0; i < messages.size(); i++){
+                Requirement require = hflow(paragraph(to_wstring(messages[i].Output())))->requirement() ;
+                temp_message = text(to_wstring(std::to_string(require.min_x) + std::to_string(require.min_y)));
+                emessages.push_back(temp_message);
+            }*/
+            //return window(text(L"Nerd Club") ,(vbox(emessages)));
+            return window(text(L"Nerd Club") ,(vbox(emessages)));
         }
 };
 
@@ -93,6 +151,7 @@ class Header : public Component{
                 text(L"Textinger") | bold | hcenter,
                 //window(text(L"InfoClub") , text(L"Hello, It's me")) | flex,
                 message_field.Render() | flex,
+                //message_field.Render() ,
                 type_field.Render() | border,
             });
         }
@@ -100,6 +159,9 @@ class Header : public Component{
 
 int main(int argc, const char* argv[]){
     auto screen = ScreenInteractive::Fullscreen();
+
+    screenw = Terminal::Size().dimx;
+    screenh = Terminal::Size().dimy;
 
     std::thread update([&screen]() {
         for(;;){
